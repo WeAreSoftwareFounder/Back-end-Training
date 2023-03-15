245f00d27a56da88be42f2d9b54db825d2f6aa32
WeAreSoftwareFounder <35461641+WeAreSoftwareFounder@users.noreply.github.com>
2023-03-15 20:22:42 +0000

    Made changes to sever
    Improved functionality and created file logging system for URL request

--- Movie_api/server.js
+++ Movie_api/server.js
@@ -1,8 +1,19 @@
 import { createServer } from 'http';
 import { appendFile, readFile } from 'fs';
 import { parse } from 'url';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 createServer((request, response) => {
+
+    const __filename = fileURLToPath(import.meta.url);
+
+    const __dirname = path.dirname(__filename);
+
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello Node!" \n')
+    console.log('My fist sever is running on: Port 8080 ')
+
     let addr = request.url,
         q = parse(addr, true),
         filePath = '';
@@ -11,7 +22,7 @@ createServer((request, response) => {
         if (err) {
             console.log(err);
         } else {
-            console.log('Added to log.');
+            console.log('Added to file');
         }
     });
 
@@ -29,8 +40,5 @@ createServer((request, response) => {
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write(data);
         response.end();
-        console.log('Server created')
-
-
-    })
+    });
 }).listen(8080);
